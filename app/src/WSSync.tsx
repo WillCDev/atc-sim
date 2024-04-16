13a1f40786f4253d51d84903ca1877afa4abd281
Will CHadwick <will.chadwick@redington.co.uk>
2024-04-16 19:59:26 +0100

    Cleanup websocket handling

--- app/src/WSSync.tsx
+++ app/src/WSSync.tsx
@@ -1,24 +1,16 @@
-import { FC, useRef } from 'react'
-import { connectToWs } from './api/ws'
+import { FC, useEffect } from 'react'
+import { WSServer } from './api/ws'
 import { useSimStore } from './store'
 
-const connection = connectToWs()
-
 export const WsSync: FC = () => {
-  const hasLoaded = useRef(false)
   const setSimData = useSimStore((state) => state.setSimData)
 
-  if (!hasLoaded.current) {
-    hasLoaded.current = true
-
-    connection.onmessage = (message) => {
-      const data = JSON.parse(message.data)
-
-      if (data.type === 'SIM_DATA') {
-        setSimData(data.payload)
-      }
+  useEffect(() => {
+    WSServer.onSimChange(setSimData)
+    return () => {
+      WSServer.unregisterSimChange(setSimData)
     }
-  }
+  }, [])
 
   return null
 }
--- app/src/api/ws.ts
+++ app/src/api/ws.ts
@@ -1,19 +1,48 @@
+import { SimState } from '@/store'
 import { wsEndpoint } from './api'
 
-export const connectToWs = () => {
-  const ws = new WebSocket(wsEndpoint)
-
-  ws.onopen = () => {
-    console.log('Connected to ws')
-  }
-
-  ws.onmessage = (message) => {
-    console.log('Received message:', message.data)
-  }
-
-  ws.onclose = () => {
-    console.log('Disconnected from ws')
-  }
-
-  return ws
+interface SimMessage {
+  type: 'SIM_DATA'
+  payload: SimState
 }
+
+class WebsocketServer {
+  private ws: WebSocket
+  private simChangeHandlers: Set<(state: SimState) => void> = new Set()
+
+  constructor() {
+    this.ws = new WebSocket(wsEndpoint)
+
+    this.ws.onopen = () => {
+      console.log('Connected to ws')
+    }
+
+    this.ws.onmessage = (message) => {
+      console.log('Received message:', message.data)
+      this.handleMessage(JSON.parse(message.data))
+    }
+
+    this.ws.onclose = () => {
+      console.log('Disconnected from ws')
+    }
+  }
+
+  private handleMessage(message: SimMessage) {
+    if (message.type !== 'SIM_DATA') return console.error('Invalid message type')
+    this.broadcastSimChange(message.payload)
+  }
+
+  private broadcastSimChange(state: SimState) {
+    this.simChangeHandlers.forEach((handler) => handler(state))
+  }
+
+  public onSimChange(handler: (state: SimState) => void) {
+    this.simChangeHandlers.add(handler)
+  }
+
+  public unregisterSimChange(handler: (state: SimState) => void) {
+    this.simChangeHandlers.delete(handler)
+  }
+}
+
+export const WSServer = new WebsocketServer()
