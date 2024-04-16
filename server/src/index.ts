af4143f67d4369311259cda6c9e66dac25a647ce
Will CHadwick <will.chadwick@redington.co.uk>
2024-04-16 17:50:55 +0100

    move pending flights API endpoints to separate file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,11 @@
 import express, { Express, Request, Response } from 'express'
 
 import dotenv from 'dotenv'
-import {
-  CreateFlightData,
-  FlightData,
-  FlightDataSchema,
-  MyResponse,
-  SimState,
-  SimStateSchema,
-} from './types'
+import { MyResponse, SimState, SimStateSchema } from './types'
 import { Sim } from './simState'
 import { flightsRouter } from './routes/flights.routes'
 import { flightState } from './flightsState'
+import { pendingFlightsRouter } from './routes/pendingFlights.routes'
 
 dotenv.config()
 
@@ -23,7 +17,6 @@ const port = process.env.PORT || 3001
 
 const simState = new Sim()
 
-const pendingFlightsEndpoint = '/api/pendingFlights'
 const simEndpoint = '/api/sim'
 
 app.use(function (_req, res, next) {
@@ -33,57 +26,7 @@ app.use(function (_req, res, next) {
 })
 
 app.use('/api/flights', flightsRouter)
-
-app.get(
-  pendingFlightsEndpoint,
-  async (_: Request, res: Response<MyResponse<FlightData[]>>) => {
-    try {
-      const flights = flightState.getPendingFlights()
-      return res.status(200).json({ data: flights })
-    } catch (err: any) {
-      console.error(err)
-      return res.status(500).json({ err: err.message })
-    }
-  }
-)
-
-app.put(
-  pendingFlightsEndpoint,
-  async (
-    req: Request<{}, {}, CreateFlightData>,
-    res: Response<MyResponse<FlightData>>
-  ) => {
-    try {
-      const { success } = FlightDataSchema.safeParse(req.body)
-      if (!success) return res.status(400).json({ err: 'Invalid Flight Data' })
-
-      const flight = flightState.upsertPendingFlight(req.body)
-      return res.status(201).json({ data: flight })
-    } catch (err: any) {
-      console.error(err)
-      return res.status(500).json({ err: err.message })
-    }
-  }
-)
-
-app.delete(
-  `${pendingFlightsEndpoint}/:callsign`,
-  async (
-    req: Request<{ callsign: string }, {}, {}>,
-    res: Response<MyResponse<FlightData[]>>
-  ) => {
-    const id = req.params.callsign
-    console.log(id)
-    try {
-      const flights = flightState.deletePendingFlight(id)
-
-      return res.status(200).json({ data: flights })
-    } catch (err) {
-      console.error(err)
-      return res.status(404).json({ err: 'Flight Not Found' })
-    }
-  }
-)
+app.use('/api/pendingFlights', pendingFlightsRouter)
 
 app.get(simEndpoint, async (_: Request, res: Response<MyResponse<SimState>>) => {
   try {
