3ee853798a1e51015e279f2d996c81ef0e043657
Will CHadwick <will.chadwick@redington.co.uk>
2024-04-23 22:37:34 +0100

    revert API to local

--- app/src/api/api.ts
+++ app/src/api/api.ts
@@ -1,10 +1,10 @@
-// const domain = 'localhost:3001'
-// export const httpEndpoint = `http://${domain}`
-// export const wsEndpoint = `ws://${domain}`
+const domain = 'localhost:3001'
+export const httpEndpoint = `http://${domain}`
+export const wsEndpoint = `ws://${domain}`
 
-const domain = 'atc-sim.onrender.com'
-export const httpEndpoint = `https://${domain}`
-export const wsEndpoint = `wss://${domain}`
+// const domain = 'atc-sim.onrender.com'
+// export const httpEndpoint = `https://${domain}`
+// export const wsEndpoint = `wss://${domain}`
 
 export interface Response<T> {
   data: T
